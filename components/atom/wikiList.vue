9070be011a3d240c5aeacb2b8ef3773a7218bc2e
umebaradon <umebaraf@gmail.com>
2020-10-01 01:16:30 +0900

    @add ファイル追加

---
+++ components/atom/wikiList.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+	div
+		nuxt-link(:to="'/wikis/' + wikiList.id" :data-id="wikiList.id")
+			span {{ wikiList.name | wikiTitle}}
+			div {{ wikiList.name }}
+			div {{ wikiList.created }}
+</template>
+
+<script>
+
+export default {
+		data: function() {
+				return {
+				}
+		},
+		components: {
+		},
+		props: {
+				wikiList: {
+						type: Object
+				}
+		},
+		methods: {
+		}
+}
+</script>
+
+<style lang="sass" scoped>
+a
+	display: block
+	text-decoration: none
+	color: #fff
+	font-weight: bold
+	height: 100%
+</style>
